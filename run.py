979b6d491cc4c041cf15f1e23aefc4fc4ab171a4
Peter Fan <peter.j.fan@gmail.com>
2015-10-29 23:14:31 -0400

    updated app to work with Google App Engine

--- run.py
+++ run.py
@@ -3,52 +3,56 @@ import flask
 import xml.etree.ElementTree as ET
 import requests
 import xmltodict
+import httplib
 
 app = flask.Flask(__name__)
 
 @app.route('/<bb_name>')
-def hello_world(bb_name):
+def send_BBaJson(bb_name):
     """Input: a biobrick name Ex. BBa_B0034 
     Output: Retrieves the xml for the part, converts it to JSON, returns an affirmative statement and dumps the JSON to file."""
     #request the part xml, turn it into an ElementTree
-    r = requests.get("http://parts.igem.org/cgi/xml/part.cgi?part="+bb_name)
-    root = ET.fromstring(r.text)
-    #parse the root into a dictionary using the recursive seek_children function.
+    # r = requests.get("http://parts.igem.org/cgi/xml/part.cgi?part="+bba_name) #uncomment this line to use the requests library instead of httplib
+    conn = httplib.HTTPConnection("parts.igem.org/cgi/xml/part.cgi?part=")
+    conn.request("GET", bb_name)
+    r = conn.getresponse()
+    root = ET.fromstring(r.read())
+    # parse the root into a dictionary using the recursive seek_children function.
     json_dict = {}
     seek_children(root, json_dict)
-    #json_dict2 = xmltodict.parse(r.text)
-    #dump the dictionary into a .json file.
-    json.dump(json_dict ,open(bb_name+".json", 'w'), sort_keys=True, indent=4)
-    return "Converted: "+bb_name+" info to JSON from XML."
-
+    # json_dict = xmltodict.parse(r.read()) # uncomment this line to use xmltodict rather than the seek_children function. 
+    # json.dump(json_dict ,open(bb_name+".json", 'w'), sort_keys=True, indent=4) # uncomment this line to dump the dictionary into a .json file.
+    return flask.jsonify(**json_dict)
 
 def seek_children(root, json_dict):
     """Input: An ET.root (the root of an XML tree), and a dictionary object.
-    Output: Recursively builds a dictionary from xml tree with a JSON-like structure."""
-    #The try case is the iterative step, the except clause is the base case.
-    new_dict = {}
-    try:
-        #If the node has children execute the below. If the node has no children then calling "root[0]" will throw an IndexError and trigger the base case.
-        if root[0] != None:
-            #This if stmnt deals with the scenario where all the children of root have the same tag name.
-            if len([tag for tag in root.findall(root[0].tag)]) > 1: 
-                child_list =[]
-                for member in root.findall(root[0].tag):
-                    child_dict = {}
-                    seek_children(member, child_dict)
-                    child_list.append(child_dict[root[0].tag])
-                json_dict[root.tag] = {root[0].tag:child_list}
-            #This else block adds the current root to the json_dictionary then calls seek_children() on each of its children.
-            else:
-                json_dict[root.tag] = new_dict
-                for child in root:
-                    seek_children(child, new_dict)
-    except IndexError: 
-    #The base case, adds the xml tag name as a key in the json_dict with the data within the tags as the value. 
+    Output: Recursively builds a dictionary from an xml tree."""
+    if len(root)==0:
+        #The base case, when the function encounters a leaf node (a tag with no sub-elements). 
         if type(root.text) == str:
             json_dict[root.tag] = root.text.strip()
         else:
             json_dict[root.tag] = None
+    else:
+        #The iterative step.
+        #If all children of the root have the same tag name, then each child is placed in a separate dictionary object. 
+        #These dict. objects are then added to a child_list object which becomes the value of the child's tag in the resulting json.
+        if len(root.findall(root[0].tag)) > 1: 
+            child_list =[]
+            #iterating through all the children and adding them to a list. 
+            for child in root:
+                child_dict = {}
+                seek_children(child, child_dict)
+                child_list.append(child_dict[root[0].tag])
+            #Adding the {child_tagname:child_list} dictionary as the child of the root tag. 
+            json_dict[root.tag] = {root[0].tag:child_list}
+        #This else block adds the current root to the json_dict argument then calls seek_children() on each of its children.
+        else:
+            new_dict = {}
+            json_dict[root.tag] = new_dict
+            for child in root:
+                seek_children(child, new_dict)
+
 
 
 if __name__ == '__main__':
